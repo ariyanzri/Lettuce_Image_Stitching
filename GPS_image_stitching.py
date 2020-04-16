9b3f804a7a289a14c17c057133c46b33886732a1
ariyanzri <ariyanzarei@email.arizona.edu>
2020-04-16 16:21:54 -0700

    detect rows on coge

--- GPS_image_stitching.py
+++ GPS_image_stitching.py
@@ -2894,7 +2894,7 @@ def detect_rows(address):
 	# print(total)
 
 	for g in patches_groups_by_rows:
-		newlist = sorted(patches_groups_by_rows[g], key=lambda x: x.GPS_coords.Center[0], reverse=True)
+		newlist = sorted(patches_groups_by_rows[g], key=lambda x: x.GPS_coords.Center[0], reverse=False)
 		for p in newlist:
 			# print(p.GPS_coords.UL_coord)
 			# plt.scatter(p.GPS_coords.UL_coord[0],p.GPS_coords.UL_coord[1],color='red')
