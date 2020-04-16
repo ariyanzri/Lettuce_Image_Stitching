fb7e4a07954773d4556c97d0723812f86f231552
ariyanzri <ariyanzarei@email.arizona.edu>
2020-04-16 14:30:58 -0700

    detect rows on coge

--- GPS_image_stitching.py
+++ GPS_image_stitching.py
@@ -2845,7 +2845,7 @@ def detect_rows(address):
 			is_new = True
 
 			for c in center_second_dim_rows:
-				if abs(center[1]-c[1]) < height_in_GPS:
+				if abs(center[1]-c[1]) < height_in_GPS/2:
 					is_new = False
 
 			if is_new:
@@ -2863,12 +2863,14 @@ def detect_rows(address):
 				patches_groups_by_rows[(round(c[0],7),round(c[1],7))].append(p)
 
 
-					
+		
 	import matplotlib.pyplot as plt
 	plt.axis('equal')
 
 	color = 'red'
 	for g in patches_groups_by_rows:
+		print('{0}: {1}'.format(g,len(patches_groups_by_rows[g])))
+		
 		if color == 'red':
 			color = 'green'
 		else:
