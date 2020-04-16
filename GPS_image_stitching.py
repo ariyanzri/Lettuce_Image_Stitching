14279d980078155edb52f70fa10d7c62dfda10ac
ariyanzri <ariyanzarei@email.arizona.edu>
2020-04-16 12:32:21 -0700

    detect rows on coge

--- GPS_image_stitching.py
+++ GPS_image_stitching.py
@@ -2859,7 +2859,7 @@ def detect_rows(address):
 
 	for p in patches:
 		for c in center_second_dim_rows:
-				if abs(p.GPS_coords.center[1]-c[1]) < height_in_GPS:
+				if abs(p.GPS_coords.Center[1]-c[1]) < height_in_GPS:
 					patches_groups_by_rows[(round(c[0],7),round(c[1],7))].append(p)
 					
 
