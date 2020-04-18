13621c88b7027d6c53cf559590951ccb775a98d1
ariyanzri <ariyanzarei@email.arizona.edu>
2020-04-18 16:34:31 -0700

    updating for generating superpatch test

--- GPS_image_stitching.py
+++ GPS_image_stitching.py
@@ -2828,6 +2828,7 @@ def recalculate_keypoints_locations(p,SIFT_folder,x_difference,y_difference):
 			lower_kp.append((k[0]+x_difference,k[1]+y_difference))
 			lower_desc.append(list(np.array(desc_tmp[i,:])))
 
+	print('patch {0} done...'.format(p.name))
 	return upper_kp,upper_desc,lower_kp,lower_desc
 
 def recalculate_keypoints_locations_helper(args):
