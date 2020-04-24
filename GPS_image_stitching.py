b59f6626a9677a81989459c4ad8ef4a97964958c
ariyanzri <ariyanzarei@email.arizona.edu>
2020-04-24 16:31:57 -0700

    updating for testing rows

--- GPS_image_stitching.py
+++ GPS_image_stitching.py
@@ -2868,8 +2868,8 @@ class SuperPatch():
 			st_y = int(y_diff/self.y_ratio_GPS_over_pixel)
 			print(st_x,st_y)
 			print(p.size)
-			# result[st_y:st_y+p.size[0],st_x:st_x+p.size[1],:] = p.rgb_img
-			cv2.rectangle(result,(st_x+1,st_y+1),(st_x+p.size[1]-1,st_y+p.size[0]-1),(0,0,255),20)
+			result[st_y:st_y+p.size[0],st_x:st_x+p.size[1],:] = p.rgb_img
+			# cv2.rectangle(result,(st_x+1,st_y+1),(st_x+p.size[1]-1,st_y+p.size[0]-1),(0,0,255),20)
 			p.del_img()
 
 
