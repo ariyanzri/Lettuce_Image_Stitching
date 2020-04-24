a4f4433e13137b047221cc706fc1a4ff4ab23b36
ariyanzri <ariyanzarei@email.arizona.edu>
2020-04-24 14:50:10 -0700

    updating for row visualization

--- GPS_image_stitching.py
+++ GPS_image_stitching.py
@@ -3049,7 +3049,7 @@ def detect_rows(address):
 			is_new = True
 
 			for c in center_second_dim_rows:
-				if abs(center[1]-c[1]) < height_in_GPS/10:
+				if abs(center[1]-c[1]) < height_in_GPS/15:
 					is_new = False
 
 			if is_new:
