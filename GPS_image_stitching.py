fb3dccb79a0a4b0f3d26929215b36f4dbe38c628
ariyanzri <ariyanzarei@email.arizona.edu>
2020-04-24 15:37:01 -0700

    updating for row visualization

--- GPS_image_stitching.py
+++ GPS_image_stitching.py
@@ -2866,6 +2866,8 @@ class SuperPatch():
 			y_diff = self.GPS_coords.UL_coord[1] - p.GPS_coords.UL_coord[1]
 			st_x = int(x_diff/self.x_ratio_GPS_over_pixel)
 			st_y = int(y_diff/self.y_ratio_GPS_over_pixel)
+			print(st_x,st_y)
+			print(p.size)
 			result[st_y:st_y+p.size[0],st_x:st_x+p.size[1],:] = p.rgb_img
 			p.del_img()
 
