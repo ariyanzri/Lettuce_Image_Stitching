a765cb6a8d77355f55801f482598b651e2ee2f17
ariyanzri <ariyanzarei@email.arizona.edu>
2020-04-27 10:17:25 -0700

    updating for testing

--- GPS_image_stitching.py
+++ GPS_image_stitching.py
@@ -3037,7 +3037,7 @@ class SuperPatch():
 	def correct_all_patches_and_self_by_H(self,H,prev_super_patch):
 
 		new_coords = get_new_GPS_Coords(self,prev_super_patch,H)
-		diff_x = self.GPS_coords.UL_coord[0]-new_coords.UL_coord[0]
+		diff_x = -self.GPS_coords.UL_coord[0]+new_coords.UL_coord[0]
 		diff_y = self.GPS_coords.UL_coord[1]-new_coords.UL_coord[1]
 		
 		for p in self.patches:
