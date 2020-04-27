98341771f1e3d0b1e7661fb912a7d2e1cf9e5d18
ariyanzri <ariyanzarei@email.arizona.edu>
2020-04-26 23:11:07 -0700

    drawing combin

--- GPS_image_stitching.py
+++ GPS_image_stitching.py
@@ -3388,7 +3388,7 @@ def generate_superpatches(groups_by_rows,SIFT_folder,patch_folder):
 	super_patches[1].correct_supper_patch_internally(SIFT_folder,patch_folder)
 	super_patches[2].correct_supper_patch_internally(SIFT_folder,patch_folder)
 
-	super_patches[2].correct_whole_based_on_super_patch(super_patches[1],SIFT_folder,patch_folder)
+	# super_patches[2].correct_whole_based_on_super_patch(super_patches[1],SIFT_folder,patch_folder)
 	# super_patches[8].draw_super_patch(patch_folder,'new')
 
 	sp = create_supper_patch_parallel(super_patches[1].patches+super_patches[2].patches,1,SIFT_folder,patch_folder)
