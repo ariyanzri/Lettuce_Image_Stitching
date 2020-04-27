81853f990999e5cc1b38a9ead43ef4693a47b92d
ariyanzri <ariyanzarei@email.arizona.edu>
2020-04-27 11:04:43 -0700

    updating for testing

--- GPS_image_stitching.py
+++ GPS_image_stitching.py
@@ -3068,9 +3068,9 @@ class SuperPatch():
 				if inner_p.has_overlap(prev_inner_p) or prev_inner_p.has_overlap(inner_p):
 					overlap1 = inner_p.get_overlap_rectangle(prev_inner_p)
 					overlap2 = prev_inner_p.get_overlap_rectangle(inner_p)
-					
+					print(overlap1)
 					if overlap1[2]-overlap1[0]<inner_p.size[1]/5 and overlap1[3]-overlap1[1]<inner_p.size[0]/5:
-						print('..')
+
 						continue
 
 					kp1,desc1 = choose_SIFT_key_points(inner_p,overlap1[0],overlap1[1],overlap1[2],overlap1[3],SIFT_folder)
