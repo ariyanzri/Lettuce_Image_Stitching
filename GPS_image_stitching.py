4ae2ff3fe0f0908a10d757a9a68da402c0e4536d
ariyanzri <ariyanzarei@email.arizona.edu>
2020-04-27 02:09:15 -0700

    drawing combin

--- GPS_image_stitching.py
+++ GPS_image_stitching.py
@@ -3277,7 +3277,7 @@ def detect_rows(address):
 	for g in patches_groups_by_rows:
 		newlist = sorted(patches_groups_by_rows[g], key=lambda x: x.GPS_coords.Center[0], reverse=False)
 		
-		patches_groups_by_rows_new[g] = newlist[:4]
+		patches_groups_by_rows_new[g] = newlist[:1]
 
 	# print(len(patches_groups_by_rows))
 	# print(len(patches_groups_by_rows[g]))
