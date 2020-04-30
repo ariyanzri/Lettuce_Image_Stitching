6d63c0b80c196fb86d2d17ef7764723422aba471
ariyanzri <ariyanzarei@email.arizona.edu>
2020-04-30 01:52:02 -0700

    for generating the test for meeting

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -944,15 +944,15 @@ def main():
 
 	elif server == 'laplace.cs.arizona.edu':
 		print('RUNNING ON -- {0} --'.format(server))
-		# os.system("taskset -p -c 1,2,3,4,5,6,7,8,9,10,11,12,14,15,16,17,18,19,20,21,22,23,24,25,27,28,29,30,31,32,33,34,35,36,37,38,39,44,45,46 %d" % os.getpid())
+		os.system("taskset -p -c 1,2,3,4,5,6,7,8,9,10,11,12,14,15,16,17,18,19,20,21,22,23,24,25,27,28,29,30,31,32,33,34,35,36,37,38,39,44,45,46 %d" % os.getpid())
 		
 		field = Field()
 
 
-		field.draw_and_save_field()
-		field.correct_field()
 		# field.draw_and_save_field()
-		field.save_new_coordinate()
+		field.correct_field()
+		field.draw_and_save_field()
+		# field.save_new_coordinate()
 
 	elif server == 'ariyan':
 		print('RUNNING ON -- {0} --'.format(server))
