4f5875152fdf439ac6dc055c9fdae41024b7554a
ariyanzri <ariyanzarei@email.arizona.edu>
2020-04-30 02:48:29 -0700

    final run on laplace after debugging

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -16,7 +16,7 @@ from collections import OrderedDict
 PATCH_SIZE = (3296, 2472)
 PATCH_SIZE_GPS = (8.899999997424857e-06,1.0199999998405929e-05)
 HEIGHT_RATIO_FOR_ROW_SEPARATION = 0.1
-NUMBER_OF_ROWS_IN_GROUPS = 3
+NUMBER_OF_ROWS_IN_GROUPS = 10
 NUMBER_OF_GOOD_MATCHES_FOR_GROUP_WISE_CORRECTION = 3000
 GPS_TO_IMAGE_RATIO = (PATCH_SIZE_GPS[0]/PATCH_SIZE[1],PATCH_SIZE_GPS[1]/PATCH_SIZE[0])
 
@@ -699,7 +699,7 @@ class Field:
 		print('Field initialized with {0} groups of {1} rows each.'.format(len(groups),NUMBER_OF_ROWS_IN_GROUPS))
 		sys.stdout.flush()
 
-		return groups[3:5]
+		return groups
 
 	def get_rows(self):
 		global coordinates_file
@@ -763,7 +763,7 @@ class Field:
 		for g in patches_groups_by_rows:
 			newlist = sorted(patches_groups_by_rows[g], key=lambda x: x.gps.Center[0], reverse=False)
 			
-			rows.append(newlist[0:5])
+			rows.append(newlist)
 
 		return rows
 
@@ -952,8 +952,8 @@ def main():
 
 		# field.draw_and_save_field()
 		field.correct_field()
-		field.draw_and_save_field()
-		# field.save_new_coordinate()
+		# field.draw_and_save_field()
+		field.save_new_coordinate()
 
 	elif server == 'ariyan':
 		print('RUNNING ON -- {0} --'.format(server))
@@ -961,7 +961,7 @@ def main():
 
 
 
-server_core = {'coge':64,'laplace.cs.arizona.edu':20,'ariyan':4}
+server_core = {'coge':64,'laplace.cs.arizona.edu':40,'ariyan':4}
 
 server = socket.gethostname()
 no_of_cores_to_use = server_core[server]
