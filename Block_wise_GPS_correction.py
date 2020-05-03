61fe83e419a0c57aaef21e514d43847f8d42c563
ariyanzri <ariyanzarei@email.arizona.edu>
2020-05-02 23:30:47 -0700

    updating for jitter testing

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -536,8 +536,8 @@ def jitter_and_calculate_dissimilarity(patch,neighbors,jx,jy):
 
 def jitter_image_to_find_least_dissimilarity(patch,neighbors):
 	
-	list_jitter_x = [-0.0000004,-0.0000003,-0.0000002,-0.0000001,0,0.0000001,0.0000002,0.0000003,0.0000004]
-	list_jitter_y = [-0.0000004,-0.0000003,-0.0000002,-0.0000001,0,0.0000001,0.0000002,0.0000003,0.0000004]
+	list_jitter_x = list(range(-0.0000009,0.0000009,0.0000001))
+	list_jitter_y = list(range(-0.0000009,0.0000009,0.0000001))
 
 	min_dissimilarity = sys.maxsize
 	min_gps = None
