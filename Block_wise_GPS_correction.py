6150bf394846dd5ddf470f69ca83f281cdad5ad9
ariyanzri <ariyanzarei@email.arizona.edu>
2020-05-03 00:07:45 -0700

    updating for jitter testing

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -536,22 +536,22 @@ def calculate_dissimilarity(p1,p2,p1_x1,p1_y1,p1_x2,p1_y2,p2_x1,p2_y1,p2_x2,p2_y
 	overlap_1_img = cv2.cvtColor(overlap_1_img, cv2.COLOR_BGR2GRAY)
 	overlap_2_img = cv2.cvtColor(overlap_2_img, cv2.COLOR_BGR2GRAY)
 
-	overlap_1_img = cv2.blur(overlap_1_img,(5,5))
-	overlap_2_img = cv2.blur(overlap_2_img,(5,5))
+	# overlap_1_img = cv2.blur(overlap_1_img,(5,5))
+	# overlap_2_img = cv2.blur(overlap_2_img,(5,5))
 
-	ret1,overlap_1_img = cv2.threshold(overlap_1_img,0,255,cv2.THRESH_OTSU)
-	ret1,overlap_2_img = cv2.threshold(overlap_2_img,0,255,cv2.THRESH_OTSU)
+	# ret1,overlap_1_img = cv2.threshold(overlap_1_img,0,255,cv2.THRESH_OTSU)
+	# ret1,overlap_2_img = cv2.threshold(overlap_2_img,0,255,cv2.THRESH_OTSU)
 
-	tmp_size = np.shape(overlap_1_img)
+	# tmp_size = np.shape(overlap_1_img)
 	
-	overlap_1_img[overlap_1_img==255] = 1
-	overlap_2_img[overlap_2_img==255] = 1
+	# overlap_1_img[overlap_1_img==255] = 1
+	# overlap_2_img[overlap_2_img==255] = 1
 
-	xnor_images = np.logical_xor(overlap_1_img,overlap_2_img)
+	# xnor_images = np.logical_xor(overlap_1_img,overlap_2_img)
 
-	dissimilarity = round(np.sum(xnor_images)/(tmp_size[0]*tmp_size[1]),2)
-	# dissimilarity =  np.sum((overlap_1_img.astype("float") - overlap_2_img.astype("float")) ** 2)
-	# dissimilarity /= float(overlap_1_img.shape[0] * overlap_1_img.shape[1])
+	# dissimilarity = round(np.sum(xnor_images)/(tmp_size[0]*tmp_size[1]),2)
+	dissimilarity =  np.sum((overlap_1_img.astype("float") - overlap_2_img.astype("float")) ** 2)
+	dissimilarity /= float(overlap_1_img.shape[0] * overlap_1_img.shape[1])
 	
 
 	return dissimilarity
