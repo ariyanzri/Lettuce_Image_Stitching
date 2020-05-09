62a83afdd1f31a022b5cdd488c49e799e80f4814
ariyanzri <ariyanzarei@email.arizona.edu>
2020-05-09 15:28:04 -0700

    testing on laplace for not using bad corrected neighbors

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -1598,7 +1598,7 @@ def main(scan_date):
 
 		# correct_patch_group_all_corrected_neighbors(field.groups[0].patches)
 
-		field.draw_and_save_field()
+		# field.draw_and_save_field()
 		# field.groups[0].correct_internally()
 		field.correct_field()
 		# field.groups[0].correct_internally()
