fbc48891b154138db1b63c700ea8dd458bfd136f
ariyanzri <ariyanzarei@email.arizona.edu>
2020-05-17 15:04:55 -0700

    testing lettuce heads method on laplace

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -1508,7 +1508,7 @@ class Patch:
 		# 	img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)		
 
 		# img  = cv2.medianBlur(img,13)
-		kernel =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
+		kernel =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 		img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)	
 
 		# kernel =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
