b850df67eb84fca350121391af7d9a93c8d18014
ariyanzri <ariyanzarei@email.arizona.edu>
2020-06-03 22:58:18 -0700

    test rmse lids after correction for results

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -852,7 +852,7 @@ def get_lid_in_patch(img_name,l,pname,coord,ransac_iter=100,ransac_min_num_fit=1
 
 	x,y,r = ransac(xs,ys,ransac_iter,ransac_min_num_fit)
 	
-	if x >= 0 and x < shp[1] and y >= 0 and y < shp[0] and r >= 400 and r <= 500:
+	if x >= 0 and x < shp[1] and y >= 0 and y < shp[0] and r >= 400 and r <= 500 or True:
 		return x,y,r,l,pname,coord
 	else:
 		print(x,y,r)
