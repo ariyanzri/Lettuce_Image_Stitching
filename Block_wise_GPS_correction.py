8d4803de06c8aade29d80387373929f33912ce07
ariyanzri <ariyanzarei@email.arizona.edu>
2020-06-06 18:09:25 -0700

    updating for testing error measure on coge

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -26,8 +26,8 @@ from collections import OrderedDict,Counter
 PATCH_SIZE = (3296, 2472)
 PATCH_SIZE_GPS = (8.899999997424857e-06,1.0199999998405929e-05)
 HEIGHT_RATIO_FOR_ROW_SEPARATION = 0.1
-# NUMBER_OF_ROWS_IN_GROUPS = 10
-NUMBER_OF_ROWS_IN_GROUPS = 4
+NUMBER_OF_ROWS_IN_GROUPS = 10
+# NUMBER_OF_ROWS_IN_GROUPS = 4
 PERCENTAGE_OF_GOOD_MATCHES_FOR_GROUP_WISE_CORRECTION = 0.5
 GPS_TO_IMAGE_RATIO = (PATCH_SIZE_GPS[0]/PATCH_SIZE[1],PATCH_SIZE_GPS[1]/PATCH_SIZE[0])
 MINIMUM_PERCENTAGE_OF_INLIERS = 0.1
@@ -2917,7 +2917,7 @@ class Field:
 		print('Field initialized with {0} groups of {1} rows each.'.format(len(groups),NUMBER_OF_ROWS_IN_GROUPS))
 		sys.stdout.flush()
 
-		return groups[4:6]
+		return groups
 
 	def get_rows(self):
 		global coordinates_file
@@ -2981,7 +2981,7 @@ class Field:
 		for g in patches_groups_by_rows:
 			newlist = sorted(patches_groups_by_rows[g], key=lambda x: x.gps.Center[0], reverse=False)
 			
-			rows.append(newlist[6:10])
+			rows.append(newlist)
 
 		print('Rows calculated and created completely.')
 
@@ -3310,8 +3310,10 @@ def main(scan_date):
 		# field.correct_field()
 		# field.draw_and_save_field()
 		# field.save_new_coordinate()
-		print("({:.10f},{:.10f})".format(calculate_error_of_correction(True)))
-		print("({:.10f},{:.10f})".format(calculate_error_of_correction()))
+		err = calculate_error_of_correction(True)
+		print("({:.10f},{:.10f})".format(err[0],err[1]))
+		err = calculate_error_of_correction()
+		print("({:.10f},{:.10f})".format(err[0],err[1]))
 
 	elif server == 'laplace.cs.arizona.edu':
 		print('RUNNING ON -- {0} --'.format(server))
