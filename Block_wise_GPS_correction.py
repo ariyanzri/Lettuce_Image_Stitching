c81ee034d4b023c7f217f71c2f56c18f3ddb09e4
ariyanzri <ariyanzarei@email.arizona.edu>
2020-06-10 02:02:05 -0700

    test rmse error measure on laplace

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -26,8 +26,8 @@ from collections import OrderedDict,Counter
 PATCH_SIZE = (3296, 2472)
 PATCH_SIZE_GPS = (8.899999997424857e-06,1.0199999998405929e-05)
 HEIGHT_RATIO_FOR_ROW_SEPARATION = 0.1
-NUMBER_OF_ROWS_IN_GROUPS = 10
-# NUMBER_OF_ROWS_IN_GROUPS = 3
+# NUMBER_OF_ROWS_IN_GROUPS = 10
+NUMBER_OF_ROWS_IN_GROUPS = 4
 PERCENTAGE_OF_GOOD_MATCHES_FOR_GROUP_WISE_CORRECTION = 0.5
 GPS_TO_IMAGE_RATIO = (PATCH_SIZE_GPS[0]/PATCH_SIZE[1],PATCH_SIZE_GPS[1]/PATCH_SIZE[0])
 MINIMUM_PERCENTAGE_OF_INLIERS = 0.1
@@ -3301,7 +3301,7 @@ class Field:
 		print('Field initialized with {0} groups of {1} rows each.'.format(len(groups),NUMBER_OF_ROWS_IN_GROUPS))
 		sys.stdout.flush()
 
-		return groups
+		return groups[4:6]
 
 	def get_rows(self,discard_right=DISCARD_RIGHT_FLAG,use_corrected=False):
 		global coordinates_file, CORRECTED_coordinates_file
@@ -3373,7 +3373,7 @@ class Field:
 		for g in patches_groups_by_rows:
 			newlist = sorted(patches_groups_by_rows[g], key=lambda x: x.gps.Center[0], reverse=False)
 			
-			rows.append(newlist)
+			rows.append(newlist[5:10])
 
 		print('Rows calculated and created completely.')
 
@@ -3639,7 +3639,7 @@ def get_approximate_random_RMSE_overlap(field,sample_no_per_group):
 
 			for n in group.patches:
 				n.load_img()
-				
+
 				if n.has_overlap(p) or p.has_overlap(n):
 
 					overlap_1,overlap_2 = p.get_overlap_rectangles(n)
