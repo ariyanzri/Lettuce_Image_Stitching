325d074d10ebc80dc470d7c2f265a2a4fcaaffa4
ariyanzri <ariyanzarei@email.arizona.edu>
2020-06-09 18:18:53 -0700

    05-19 old method on HPC for test scans

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -3831,8 +3831,8 @@ method = 'Old_method'
 # scan_date = '2020-02-18'
 # scan_date = '2020-01-08'
 
-scan_date = '2020-05-18'
-# scan_date = '2020-05-19'
+# scan_date = '2020-05-18'
+scan_date = '2020-05-19'
 
 print('Starting process on {0} for scan date {1} using method {2}.'.format(server,scan_date,method))
 
