9684e1ddfbf9f8c116d7f4c86e3e675404aa7753
ariyanzri <ariyanzarei@email.arizona.edu>
2020-06-14 15:14:54 -0700

    run small scale coge for new SIFT test

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -3898,7 +3898,7 @@ def main(scan_date):
 		# field.create_patches_SIFT_files()
 
 		# field.groups[14].correct_internally()
-		# field.draw_and_save_field(is_old=True)
+		field.draw_and_save_field(is_old=True)
 		field.correct_field()
 		field.draw_and_save_field(is_old=False)
 		# field.save_new_coordinate()
