26ed4f81631c59e18f73d6ff14a03c357f394981
ariyanzri <ariyanzarei@email.arizona.edu>
2020-06-18 14:26:43 -0700

    testing a lot of things on laplace

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -32,14 +32,14 @@ from collections import OrderedDict,Counter
 # PATCH_SIZE = (330, 247) # 0.1
 # SCALE = 0.1
 
-PATCH_SIZE = (659, 494) # 0.2
-SCALE = 0.2
+# PATCH_SIZE = (659, 494) # 0.2
+# SCALE = 0.2
 
 # PATCH_SIZE = (989, 742) # 0.3
 # SCALE = 0.3
 
-# PATCH_SIZE = (1318, 989) # 0.4
-# SCALE = 0.4
+PATCH_SIZE = (1318, 989) # 0.4
+SCALE = 0.4
 
 # PATCH_SIZE = (1648, 1236) # 0.5 
 # SCALE = 0.5
