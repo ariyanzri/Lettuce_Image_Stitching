a839f670f546d2276c47c18cc5a8fea485a806a5
ariyanzri <ariyanzarei@email.arizona.edu>
2020-06-18 23:19:21 -0700

    test laplace

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -4363,7 +4363,7 @@ method = 'MST'
 
 
 # scan_date = '2020-02-18'
-# scan_date = '2020-01-08'
+scan_date = '2020-01-08'
 # scan_date = '2020-05-18'
 # scan_date = '2020-05-19'
 # scan_date = '2020-06-02'
@@ -4376,7 +4376,7 @@ method = 'MST'
 # scan_date = '2020-06-05_hardware_north'
 # scan_date = '2020-06-05_hardware_south'
 # scan_date = 'hardware_f6,7_summer_shade'
-scan_date = 'hardware_f6,7_summer_suntest061620'
+# scan_date = 'hardware_f6,7_summer_suntest061620'
 # scan_date = 'software_f6,7_summer_shade'
 
 
