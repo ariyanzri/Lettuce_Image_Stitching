1272a60a77e1f33d566a5235e8b13b574c0a7302
ariyanzri <ariyanzarei@email.arizona.edu>
2020-06-20 22:18:51 -0700

    lid base err test

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -2497,7 +2497,10 @@ class Patch:
 		img[img>=150] = 255
 		img[img<150] = 0
 
-		img  = cv2.medianBlur(img,int(17*SCALE))	
+		img  = cv2.medianBlur(img,int(17*SCALE))
+
+		kernel =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (int(50*SCALE),int(50*SCALE)))
+		img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)	
 
 		kernel =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (int(50*SCALE),int(50*SCALE)))
 		img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)		
