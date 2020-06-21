7ff30fb69fd35dafb61fc17de6c2806d8123db03
ariyanzri <ariyanzarei@email.arizona.edu>
2020-06-21 00:46:38 -0700

    test laplace

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -4410,15 +4410,15 @@ def main(scan_date):
 		field = Field()
 		field.detect_lid_patches()
 		print(field.calculate_lid_based_error())
-		# cv2.namedWindow('fig3',cv2.WINDOW_NORMAL)
-		# cv2.resizeWindow('fig3', 700,700)
+		cv2.namedWindow('fig3',cv2.WINDOW_NORMAL)
+		cv2.resizeWindow('fig3', 700,700)
 
-		# for p,l,x,y in field.detected_lid_patches:
-		# 	p.load_img()
-		# 	print(l)
-		# 	cv2.circle(p.rgb_img,(x,y),10,(0,0,255),-1)
-		# 	cv2.imshow('fig3',p.rgb_img)
-		# 	cv2.waitKey(0)
+		for p,l,x,y in field.detected_lid_patches:
+			p.load_img()
+			print(l)
+			cv2.circle(p.rgb_img,(x,y),10,(0,0,255),-1)
+			cv2.imshow('fig3',p.rgb_img)
+			cv2.waitKey(0)
 
 	elif server == 'ariyan':
 		print_settings()
