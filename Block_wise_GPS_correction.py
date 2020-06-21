run 02-18 on coge with good settings after fixing lid based error and removing scale and rotation bad matches

Block_wise_GPS_correction.py
@@ -4279,38 +4279,43 @@ def main(scan_date):
 		# Corrections
 		# print('------------------ BEGINNING CORRECTION ------------------ ')
 
+		lettuce_coords = read_lettuce_heads_coordinates()
+
 		field = Field()
-		# lettuce_coords = read_lettuce_heads_coordinates()
+		
+		old_lid_base_error = field.calculate_lid_based_error()
+
 		field.create_patches_SIFT_files()
 		
 		field.draw_and_save_field(is_old=True)
+
 		field.correct_field()
+
 		field.draw_and_save_field(is_old=False)
+
 		field.save_new_coordinate()
 
+		new_lid_base_error = field.calculate_lid_based_error()
 
-		# Measure Errors after correction
-		# print('------------------ ERROR MEASUREMENT ------------------ ')
+		print('------------------ ERROR MEASUREMENT ------------------ ')
 
-		# print('*** Before')
+		print('*** Before')
 
-		# err = calculate_error_of_correction(True)
-		# print("({:.10f},{:.10f})".format(err[0],err[1]))
+		print('Lid base Mean and Stdev: {0}'.format(old_lid_base_error))
 
-		# field = Field(False)
-		# res = get_approximate_random_RMSE_overlap(field,10,no_of_cores_to_use_max)
-		# np.save('RMSE_before.npy',res)
-		# print(np.mean(res[:,3]))
+		field = Field(False)
+		res = get_approximate_random_RMSE_overlap(field,10,no_of_cores_to_use_max)
+		np.save('RMSE_before.npy',res)
+		print(np.mean(res[:,3]))
 
-		# print('*** After')
+		print('*** After')
 
-		# err = calculate_error_of_correction()
-		# print("({:.10f},{:.10f})".format(err[0],err[1]))
+		print('Lid base Mean and Stdev: {0}'.format(new_lid_base_error))
 
-		# field = Field(True)
-		# res = get_approximate_random_RMSE_overlap(field,10,no_of_cores_to_use_max)
-		# np.save('RMSE_after.npy',res)
-		# print(np.mean(res[:,3]))
+		field = Field(True)
+		res = get_approximate_random_RMSE_overlap(field,10,no_of_cores_to_use_max)
+		np.save('RMSE_after.npy',res)
+		print(np.mean(res[:,3]))
 
 
 	elif server == 'laplace.cs.arizona.edu':
@@ -4498,8 +4503,8 @@ method = 'MST'
 # method = 'Old_method'
 
 
-# scan_date = '2020-02-18'
-scan_date = '2020-01-08'
+scan_date = '2020-02-18'
+# scan_date = '2020-01-08'
 # scan_date = '2020-05-18'
 # scan_date = '2020-05-19'
 # scan_date = '2020-06-02'