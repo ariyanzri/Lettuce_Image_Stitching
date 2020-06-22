1403847338a0adbf4fc0b507371bbf36135634af
ariyanzri <ariyanzarei@email.arizona.edu>
2020-06-21 18:08:14 -0700

    HPC scale 0.2

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -4516,13 +4516,13 @@ GPS_ERROR_X = 0.000001
 FFT_PARALLEL_CORES_TO_USE = 20
 
 
-number_of_rows_in_groups = 10
-groups_to_use = slice(0,None)
-patches_to_use = slice(0,None)
+# number_of_rows_in_groups = 10
+# groups_to_use = slice(0,None)
+# patches_to_use = slice(0,None)
 
-# number_of_rows_in_groups = 3
-# groups_to_use = slice(0,4)
-# patches_to_use = slice(0,30)
+number_of_rows_in_groups = 3
+groups_to_use = slice(0,4)
+patches_to_use = slice(0,30)
 
 
 inside_radius_lettuce_matching_threshold = 200*SCALE
