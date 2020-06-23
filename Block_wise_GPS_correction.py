ce1a00a7e9b9a3c3d5d1e555c2e3d36add1cdf22
ariyanzri <ariyanzarei@email.arizona.edu>
2020-06-22 22:42:36 -0700

    hybrid mst on coge 02-18 latest

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -4479,35 +4479,35 @@ def main(scan_date):
 
 		field = Field()
 		
-		field.save_plot()
+		# field.save_plot()
 
-		# old_lid_base_error = field.calculate_lid_based_error()
+		old_lid_base_error = field.calculate_lid_based_error()
 
-		# old_RMSE = get_approximate_random_RMSE_overlap(field,10,no_of_cores_to_use_max)
+		old_RMSE = get_approximate_random_RMSE_overlap(field,10,no_of_cores_to_use_max)
 
-		# field.create_patches_SIFT_files()
+		field.create_patches_SIFT_files()
 		
-		# # field.draw_and_save_field(is_old=True)
+		# field.draw_and_save_field(is_old=True)
 
-		# field.correct_field()
+		field.correct_field()
 
-		# field.draw_and_save_field(is_old=False)
+		field.draw_and_save_field(is_old=False)
 
-		# field.save_new_coordinate()
-
-
-		# new_lid_base_error = field.calculate_lid_based_error()
-		# new_RMSE = get_approximate_random_RMSE_overlap(field,10,no_of_cores_to_use_max)
-
-		# print('------------------ ERROR MEASUREMENT ------------------ ')
+		field.save_new_coordinate()
 
 
-		# print('OLD Lid base Mean and Stdev: {0}'.format(old_lid_base_error))
-		# print('OLD SI: {0}'.format(np.mean(old_RMSE[:,3])))
+		new_lid_base_error = field.calculate_lid_based_error()
+		new_RMSE = get_approximate_random_RMSE_overlap(field,10,no_of_cores_to_use_max)
+
+		print('------------------ ERROR MEASUREMENT ------------------ ')
+
+
+		print('OLD Lid base Mean and Stdev: {0}'.format(old_lid_base_error))
+		print('OLD SI: {0}'.format(np.mean(old_RMSE[:,3])))
 		
 
-		# print('NEW Lid base Mean and Stdev: {0}'.format(new_lid_base_error))
-		# print('NEW SI: {0}'.format(np.mean(new_RMSE[:,3])))
+		print('NEW Lid base Mean and Stdev: {0}'.format(new_lid_base_error))
+		print('NEW SI: {0}'.format(np.mean(new_RMSE[:,3])))
 
 
 	elif server == 'laplace.cs.arizona.edu':
@@ -4694,8 +4694,8 @@ method = 'HybridMST'
 # method = 'Old_method'
 
 
-# scan_date = '2020-02-18'
-scan_date = '2020-01-08'
+scan_date = '2020-02-18'
+# scan_date = '2020-01-08'
 # scan_date = '2020-05-18'
 # scan_date = '2020-05-19'
 # scan_date = '2020-06-02'
