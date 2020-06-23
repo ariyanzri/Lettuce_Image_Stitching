722bdd9d031786e3a302aa688a9b5de82db3e655
ariyanzri <ariyanzarei@email.arizona.edu>
2020-06-22 22:21:47 -0700

    test plot

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -3683,8 +3683,12 @@ class Field:
 			group = Group(len(groups),row_window)
 			groups.append(group)
 
-			start = end
+			start = end - 1
 			end = start + number_of_rows_in_groups -1
+
+		# while True:
+
+		# 	rows[:number_of_rows_in_groups]
 
 		print('Field initialized with {0} groups of {1} rows each.'.format(len(groups),number_of_rows_in_groups))
 		sys.stdout.flush()
