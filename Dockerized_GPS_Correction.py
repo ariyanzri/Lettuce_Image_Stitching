ba634e858db2233b14be977f703c25671c465c9f
ariyanzri <ariyanzarei@email.arizona.edu>
2020-06-28 17:02:32 -0700

    adding dockerized file and config file

---
+++ Dockerized_GPS_Correction.py
@@ -0,0 +1,133 @@
+import numpy as np
+import cv2
+import random
+import math
+import multiprocessing
+import datetime
+import sys
+import gc
+import pickle
+import os
+import threading
+import socket
+import statistics
+import datetime
+from Customized_myltiprocessing import MyPool
+from heapq import heappush, heappop, heapify
+from collections import OrderedDict,Counter
+
+from Block_wise_GPS_correction import *
+
+
+def initialize_settings(scan_date,config_file,local_address)
+	global method,no_of_cores_to_use,no_of_cores_to_use_max,SCALE,PATCH_SIZE,LID_SIZE_AT_SCALE,PATCH_SIZE_GPS,\
+	GPS_TO_IMAGE_RATIO,HEIGHT_RATIO_FOR_ROW_SEPARATION,PERCENTAGE_OF_GOOD_MATCHES,MINIMUM_PERCENTAGE_OF_INLIERS,\
+	MINIMUM_NUMBER_OF_MATCHES,RANSAC_MAX_ITER,RANSAC_ERROR_THRESHOLD,PERCENTAGE_NEXT_NEIGHBOR_FOR_MATCHES,\
+	OVERLAP_DISCARD_RATIO,TRANSFORMATION_SCALE_DISCARD_THRESHOLD,TRANSFORMATION_ANGLE_DISCARD_THRESHOLD,\
+	LETTUCE_AREA_THRESHOLD,CONTOUR_MATCHING_MIN_MATCH,ORTHO_SCALE,REDUCTION_FACTOR,OPEN_MORPH_LID_SIZE,\
+	CLOSE_MORPH_LID_SIZE,GPS_ERROR_Y,GPS_ERROR_X,FFT_PARALLEL_CORES_TO_USE,discard_right_flag,override_sifts,\
+	patch_folder,SIFT_folder,lid_file,coordinates_file,CORRECTED_coordinates_file,plot_npy_file,\
+	row_save_path,field_image_path,lettuce_heads_coordinates_file,method,correction_log_file,inside_radius_lettuce_matching_threshold,\
+	number_of_rows_in_groups,groups_to_use,patches_to_use
+
+	with open(config_file,'r') as f:
+		lines = f.read().split('\n')
+
+		method = lines[0].split(':')[1]
+		no_of_cores_to_use = int(lines[1].split(':')[1])
+		no_of_cores_to_use_max = int(lines[2].split(':')[1])
+		SCALE = float(lines[3].split(':')[1])
+		PATCH_SIZE = (int(3296*SCALE),int(2472*SCALE))
+		LID_SIZE_AT_SCALE = (400*SCALE,600*SCALE)
+		PATCH_SIZE_GPS = (-1,-1)
+		GPS_TO_IMAGE_RATIO = (PATCH_SIZE_GPS[0]/PATCH_SIZE[1],PATCH_SIZE_GPS[1]/PATCH_SIZE[0])
+		HEIGHT_RATIO_FOR_ROW_SEPARATION = float(lines[4].split(':')[1])
+		PERCENTAGE_OF_GOOD_MATCHES = float(lines[5].split(':')[1])
+		MINIMUM_PERCENTAGE_OF_INLIERS = float(lines[6].split(':')[1])
+		MINIMUM_NUMBER_OF_MATCHES = int(lines[7].split(':')[1])
+		RANSAC_MAX_ITER = int(lines[8].split(':')[1])
+		RANSAC_ERROR_THRESHOLD = int(lines[9].split(':')[1])
+		PERCENTAGE_NEXT_NEIGHBOR_FOR_MATCHES = float(lines[10].split(':')[1])
+		OVERLAP_DISCARD_RATIO = float(lines[11].split(':')[1])
+		TRANSFORMATION_SCALE_DISCARD_THRESHOLD = float(lines[12].split(':')[1])
+		TRANSFORMATION_ANGLE_DISCARD_THRESHOLD = float(lines[13].split(':')[1])
+		LETTUCE_AREA_THRESHOLD = int(lines[14].split(':')[1])
+		CONTOUR_MATCHING_MIN_MATCH = int(lines[15].split(':')[1])
+		ORTHO_SCALE = float(lines[16].split(':')[1])
+		REDUCTION_FACTOR = ORTHO_SCALE/SCALE
+		OPEN_MORPH_LID_SIZE = int(lines[17].split(':')[1])
+		CLOSE_MORPH_LID_SIZE = int(lines[18].split(':')[1])
+		GPS_ERROR_Y = float(lines[19].split(':')[1])
+		GPS_ERROR_X = float(lines[20].split(':')[1])
+		FFT_PARALLEL_CORES_TO_USE = int(lines[21].split(':')[1])
+		discard_right_flag = bool(lines[22].split(':')[1])
+		override_sifts = bool(lines[23].split(':')[1])
+		inside_radius_lettuce_matching_threshold = 200*SCALE
+		number_of_rows_in_groups = int(lines[24].split(':')[1])
+		groups_to_use = slice(0,None)
+		patches_to_use = slice(0,None)
+
+		patch_folder = 'app/{0}/{1}-rgb/bin2tif_out'.format(local_address,scan_date)
+		SIFT_folder = 'app/{0}/{1}-rgb/SIFT'.format(local_address,scan_date)
+		lid_file = 'app/{0}/{1}-rgb/lids.txt'.format(local_address,scan_date)
+		coordinates_file = 'app/{0}/{1}-rgb/{2}_coordinates.csv'.format(local_address,scan_date,scan_date)
+		CORRECTED_coordinates_file = 'app/{0}/{1}-rgb/{2}_coordinates_CORRECTED.csv'.format(local_address,scan_date,scan_date)
+		plot_npy_file = 'app/{0}/{1}-rgb/plt.npy'.format(local_address,scan_date)
+		row_save_path = 'app/{0}/{1}-rgb/rows'.format(local_address,scan_date)
+		field_image_path = 'app/{0}/{1}-rgb'.format(local_address,scan_date)
+		correction_log_file = 'app/{0}/{1}-rgb/logs/log_{2}_at_{3}.csv'.format(local_address,scan_date,method,datetime.datetime.now().strftime("%d-%m-%y_%H:%M"))
+		lettuce_heads_coordinates_file = 'app/{0}/{1}-rgb/season10_ind_lettuce_2020-05-27.csv'.format(local_address,scan_date)
+
+
+def main(scan_date):
+	global lettuce_coords
+
+	lettuce_coords = read_lettuce_heads_coordinates()
+
+	field = Field()
+	
+	field.save_plot()
+
+	old_lid_base_error = field.calculate_lid_based_error()
+
+	old_RMSE = get_approximate_random_RMSE_overlap(field,10,no_of_cores_to_use_max)
+
+	field.create_patches_SIFT_files()
+	
+	field.draw_and_save_field(is_old=True)
+
+	field.correct_field()
+
+	field.draw_and_save_field(is_old=False)
+
+	field.save_new_coordinate()
+
+
+	new_lid_base_error = field.calculate_lid_based_error()
+	new_RMSE = get_approximate_random_RMSE_overlap(field,10,no_of_cores_to_use_max)
+
+	print('------------------ ERROR MEASUREMENT ------------------ ')
+
+
+	print('OLD Lid base Mean and Stdev: {0}'.format(old_lid_base_error))
+	print('OLD SI: {0}'.format(np.mean(old_RMSE[:,3])))
+	
+
+	print('NEW Lid base Mean and Stdev: {0}'.format(new_lid_base_error))
+	print('NEW SI: {0}'.format(np.mean(new_RMSE[:,3])))
+
+
+
+
+start_time = datetime.datetime.now()
+
+scan_date = sys.argv[1]
+config_file = sys.argv[2]
+local_address = sys.argv[3]
+
+initialize_settings(scan_date,config_file,local_address)
+main(scan_date)
+
+end_time = datetime.datetime.now()
+
+report_time(start_time,end_time)
