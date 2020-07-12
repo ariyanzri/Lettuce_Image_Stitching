fbefebda5649e203db0b63addb24c9f89b53be1b
ariyanzri <ariyanzarei@email.arizona.edu>
2020-07-12 15:08:14 -0700

    test HPC with unbounded

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -2305,8 +2305,8 @@ class Global_Optimizer:
 				# print(n.name)
 
 				# coef = 10*(1-params.dissimilarity)
-				# coef = (1/0.000002)*(1-params.dissimilarity)
-				coef = 1
+				coef = (1/0.000002)*(1-params.dissimilarity)
+				# coef = 1
 				# coef = int(math.sqrt(params.percentage_inliers*params.num_matches))
 				
 				row_x = - coef*template[self.image_name_to_index_dict[p.name],:] + coef*template[self.image_name_to_index_dict[n.name],:]
@@ -2319,20 +2319,20 @@ class Global_Optimizer:
 				A.append(row_y)
 				b.append(coef*diff[1])
 
-			coef = 0.5
+			# coef = 0.5
 
-			row_x = coef*template[self.image_name_to_index_dict[p.name],:]
-			row_y = coef*template[self.number_of_images + self.image_name_to_index_dict[p.name],:]
+			# row_x = coef*template[self.image_name_to_index_dict[p.name],:]
+			# row_y = coef*template[self.number_of_images + self.image_name_to_index_dict[p.name],:]
 
-			A.append(row_x)
-			b.append(coef*p.gps.UL_coord[0])
+			# A.append(row_x)
+			# b.append(coef*p.gps.UL_coord[0])
 			
 
-			A.append(row_y)
-			b.append(coef*p.gps.UL_coord[1])
+			# A.append(row_y)
+			# b.append(coef*p.gps.UL_coord[1])
 
-			LB[self.image_name_to_index_dict[p.name]] = p.gps.UL_coord[0]-settings.GPS_ERROR_X
-			UB[self.image_name_to_index_dict[p.name]] = p.gps.UL_coord[0]+settings.GPS_ERROR_X
+			LB[self.image_name_to_index_dict[p.name]] = p.gps.UL_coord[0]-10*settings.GPS_ERROR_X
+			UB[self.image_name_to_index_dict[p.name]] = p.gps.UL_coord[0]+10*settings.GPS_ERROR_X
 			LB[self.number_of_images + self.image_name_to_index_dict[p.name]] = p.gps.UL_coord[1]-settings.GPS_ERROR_Y
 			UB[self.number_of_images + self.image_name_to_index_dict[p.name]] = p.gps.UL_coord[1]+settings.GPS_ERROR_Y
 
@@ -2346,12 +2346,12 @@ class Global_Optimizer:
 
 		# X = np.matmul(np.matmul(np.linalg.inv(np.matmul(np.transpose(A),A)),np.transpose(A)),b)
 		
-		# res = lsq_linear(A, b, bounds=(LB,UB),max_iter=len(self.patches),verbose=2)
+		res = lsq_linear(A, b, bounds=(LB,UB),max_iter=len(self.patches),verbose=2)
 		# res = lsq_linear(A, b,max_iter=len(self.patches),verbose=2)
-		# X = res.x
-		# print(res.status,res.message,res.success)
+		X = res.x
+		print(res.status,res.message,res.success)
 
-		X = lsqr(A,b)[0]
+		# X = lsqr(A,b)[0]
 
 
 		for p in self.patches:
