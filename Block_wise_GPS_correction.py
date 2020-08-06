f67fcccbaf63c0d517738cc80739bc3625705dd4
ariyanzri <ariyanzarei@email.arizona.edu>
2020-08-06 13:40:08 -0700

    test seson 11

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -2265,10 +2265,10 @@ class Global_Optimizer:
 		A = []
 		b = []
 
-		transformation_coef_x = 1/(9.32*1e-6) 
-		transformation_coef_y = 1/(10.56*1e-6) 
-		GPS_coef_x = 1/(9.02*1e-5)
-		GPS_coef_y = 1/(10.48*1e-5)
+		transformation_coef_x = 1/(9.32*1e-8) 
+		transformation_coef_y = 1/(10.56*1e-8) 
+		GPS_coef_x = 1/(9.02*1e-6)
+		GPS_coef_y = 1/(10.48*1e-6)
 		
 		# GPS_lids = 1/(1e-9)
 		GPS_lids = 1/(9.02*1e-6) 
