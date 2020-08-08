e76dfd34c23f3e1c213ef11a3cfa41b6f7c4f988
ariyanzri <ariyanzarei@email.arizona.edu>
2020-08-08 14:34:38 -0700

    test season 11 global opt

--- test_main.py
+++ test_main.py
@@ -23,43 +23,43 @@ def main(scan_date):
 
 	sys.setrecursionlimit(10**6)
 	
-	# settings.lettuce_coords = read_lettuce_heads_coordinates()
+	settings.lettuce_coords = read_lettuce_heads_coordinates()
 
-	# field = Field(is_single_group=settings.is_single_group)
+	field = Field(is_single_group=settings.is_single_group)
 	
-	# field.groups[0].get_overlap_averages()
+	field.groups[0].get_overlap_averages()
 	
-	# field.save_plot()
+	field.save_plot()
 
-	# # old_lid_base_error = field.calculate_lid_based_error()
+	# old_lid_base_error = field.calculate_lid_based_error()
 
-	# old_RMSE = get_approximate_random_RMSE_overlap(field,20,settings.no_of_cores_to_use_max)
+	old_RMSE = get_approximate_random_RMSE_overlap(field,20,settings.no_of_cores_to_use_max)
 
-	# field.create_patches_SIFT_files()
+	field.create_patches_SIFT_files()
 	
-	# field.draw_and_save_field(is_old=True)
+	field.draw_and_save_field(is_old=True)
 
-	# field.correct_field()
+	field.correct_field()
 
-	# field.save_new_coordinate()
+	field.save_new_coordinate()
 
-	# field.draw_and_save_field(is_old=False)
+	field.draw_and_save_field(is_old=False)
 
-	# new_lid_base_error = field.calculate_lid_based_error()
-	# new_RMSE = get_approximate_random_RMSE_overlap(field,20,settings.no_of_cores_to_use_max)
+	new_lid_base_error = field.calculate_lid_based_error()
+	new_RMSE = get_approximate_random_RMSE_overlap(field,20,settings.no_of_cores_to_use_max)
 
-	# print('------------------ ERROR MEASUREMENT ------------------ ')
+	print('------------------ ERROR MEASUREMENT ------------------ ')
 
 
-	# # print('OLD Lid base Mean and Stdev: {0}'.format(old_lid_base_error))
-	# print('OLD SI: {0}'.format(np.mean(old_RMSE[:,3])))
+	# print('OLD Lid base Mean and Stdev: {0}'.format(old_lid_base_error))
+	print('OLD SI: {0}'.format(np.mean(old_RMSE[:,3])))
 	
 
-	# # print('NEW Lid base Mean and Stdev: {0}'.format(new_lid_base_error))
-	# print('NEW SI: {0}'.format(np.mean(new_RMSE[:,3])))
+	# print('NEW Lid base Mean and Stdev: {0}'.format(new_lid_base_error))
+	print('NEW SI: {0}'.format(np.mean(new_RMSE[:,3])))
 
-	field_new = Field(is_single_group=settings.is_single_group,use_corrected=True)
-	field_new.draw_and_save_field(is_old=False)
+	# field_new = Field(is_single_group=settings.is_single_group,use_corrected=True)
+	# field_new.draw_and_save_field(is_old=False)
 
 
 
