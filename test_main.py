b1681d3cd305e3a14b5600d5059f38430cd44914
ariyanzri <ariyanzarei@email.arizona.edu>
2020-08-09 12:48:40 -0700

    test global opt with both cameras season 11

--- test_main.py
+++ test_main.py
@@ -27,8 +27,6 @@ def main(scan_date):
 
 	field = Field(is_single_group=settings.is_single_group)
 	
-	field.groups[0].get_overlap_averages()
-	
 	field.save_plot()
 
 	# old_lid_base_error = field.calculate_lid_based_error()
@@ -37,13 +35,13 @@ def main(scan_date):
 
 	field.create_patches_SIFT_files()
 	
-	field.draw_and_save_field(is_old=True)
+	# field.draw_and_save_field(is_old=True)
 
 	field.correct_field()
 
 	field.save_new_coordinate()
 
-	field.draw_and_save_field(is_old=False)
+	# field.draw_and_save_field(is_old=False)
 
 	# new_lid_base_error = field.calculate_lid_based_error()
 	new_RMSE = get_approximate_random_RMSE_overlap(field,20,settings.no_of_cores_to_use_max)
@@ -61,6 +59,11 @@ def main(scan_date):
 	# field_new = Field(is_single_group=settings.is_single_group,use_corrected=True)
 	# field_new.draw_and_save_field(is_old=False)
 
+def save_ortho():
+	gc.collect()
+
+	field_new = Field(is_single_group=settings.is_single_group,use_corrected=True)
+	field_new.draw_and_save_field(is_old=False)
 
 
 start_time = datetime.datetime.now()
@@ -74,6 +77,7 @@ settings.initialize_settings_test(scan_date,config_file,local_address,None,None)
 
 print_settings()
 main(scan_date)
+save_ortho()
 
 end_time = datetime.datetime.now()
 
