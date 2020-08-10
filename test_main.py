be52d1a5a625635fcd0ed05a8214f60428612c91
ariyanzri <ariyanzarei@email.arizona.edu>
2020-08-09 21:48:46 -0700

    test season 11 global opt both cameras

--- test_main.py
+++ test_main.py
@@ -26,35 +26,35 @@ def main(scan_date):
 	settings.lettuce_coords = read_lettuce_heads_coordinates()
 
 	field = Field(is_single_group=settings.is_single_group)
-	
+
 	field.save_plot()
 
-	# old_lid_base_error = field.calculate_lid_based_error()
+	old_lid_base_error = field.calculate_lid_based_error()
 
-	# old_RMSE = get_approximate_random_RMSE_overlap(field,20,settings.no_of_cores_to_use_max)
+	old_RMSE = get_approximate_random_RMSE_overlap(field,20,settings.no_of_cores_to_use_max)
 
-	# field.create_patches_SIFT_files()
+	field.create_patches_SIFT_files()
 	
-	# field.draw_and_save_field(is_old=True)
+	field.draw_and_save_field(is_old=True)
 
-	# field.correct_field()
+	field.correct_field()
 
-	# field.save_new_coordinate()
+	field.save_new_coordinate()
 
-	# # field.draw_and_save_field(is_old=False)
+	# field.draw_and_save_field(is_old=False)
 
-	# # new_lid_base_error = field.calculate_lid_based_error()
-	# new_RMSE = get_approximate_random_RMSE_overlap(field,20,settings.no_of_cores_to_use_max)
+	new_lid_base_error = field.calculate_lid_based_error()
+	new_RMSE = get_approximate_random_RMSE_overlap(field,20,settings.no_of_cores_to_use_max)
 
-	# print('------------------ ERROR MEASUREMENT ------------------ ')
+	print('------------------ ERROR MEASUREMENT ------------------ ')
 
 
-	# # print('OLD Lid base Mean and Stdev: {0}'.format(old_lid_base_error))
-	# print('OLD SI: {0}'.format(np.mean(old_RMSE[:,3])))
+	print('OLD Lid base Mean and Stdev: {0}'.format(old_lid_base_error))
+	print('OLD SI: {0}'.format(np.mean(old_RMSE[:,3])))
 	
 
-	# # print('NEW Lid base Mean and Stdev: {0}'.format(new_lid_base_error))
-	# print('NEW SI: {0}'.format(np.mean(new_RMSE[:,3])))
+	print('NEW Lid base Mean and Stdev: {0}'.format(new_lid_base_error))
+	print('NEW SI: {0}'.format(np.mean(new_RMSE[:,3])))
 
 	# field_new = Field(is_single_group=settings.is_single_group,use_corrected=True)
 	# field_new.draw_and_save_field(is_old=False)
@@ -65,6 +65,8 @@ def save_ortho():
 	field_new = Field(is_single_group=settings.is_single_group,use_corrected=True)
 	field_new.draw_and_save_field(is_old=False)
 
+# settings.plot_npy_file = '/home/ariyan/Desktop/plt.npy'
+# visualize_plot()
 
 start_time = datetime.datetime.now()
 
