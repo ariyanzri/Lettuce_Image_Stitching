570e73b745f56b1a2a5b67d51fad3c4b18ac190e
ariyanzri <ariyanzarei@email.arizona.edu>
2020-08-19 22:52:56 -0700

    test lids for season 11

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -1005,7 +1005,7 @@ def get_lid_in_patch(img_name,l,pname,coord,ransac_iter=500,ransac_min_num_fit=1
 
 	# max_intensity = np.amax(img)
 	
-	t = 250
+	t = 240
 	
 	(thresh, img) = cv2.threshold(img, t, 255, cv2.THRESH_BINARY)
 
@@ -1016,11 +1016,13 @@ def get_lid_in_patch(img_name,l,pname,coord,ransac_iter=500,ransac_min_num_fit=1
 	# cv2.waitKey(0)
 
 	kernel =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (int(settings.LID_SIZE_AT_SCALE[0]*0.8), int(settings.LID_SIZE_AT_SCALE[0]*0.8)))
-	
+
 	# kernel =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (int(settings.OPEN_MORPH_LID_SIZE*settings.SCALE), int(settings.OPEN_MORPH_LID_SIZE*settings.SCALE)))
 	img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 
 	# cv2.imwrite('/storage/ariyanzarei/{0}_3.jpg'.format(img_name.split('.')[0]),img)
+
+	kernel =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (int(settings.LID_SIZE_AT_SCALE[0]*0.7), int(settings.LID_SIZE_AT_SCALE[0]*0.7)))
 
 	# kernel =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (int(settings.CLOSE_MORPH_LID_SIZE*settings.SCALE), int(settings.CLOSE_MORPH_LID_SIZE*settings.SCALE)))
 	img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)	
