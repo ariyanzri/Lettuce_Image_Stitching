7ffc429d768f5fca34e7642415d235d40e0a21eb
ariyanzri <ariyanzarei@email.arizona.edu>
2020-08-19 23:43:51 -0700

    final modifications to fix the lid problem and season 11 ortho problem

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -1069,8 +1069,8 @@ def get_lid_in_patch(img_name,l,pname,coord,ransac_iter=500,ransac_min_num_fit=1
 	# cv2.imwrite('tmp-{0}-{1}.jpg'.format(x,y),rgb_img)
 
 	if r >= settings.LID_SIZE_AT_SCALE[0] and r <= settings.LID_SIZE_AT_SCALE[1]:
-		cv2.circle(rgb_img,(x,y),r,(0,255,0),thickness=5)
-		cv2.imwrite('/storage/ariyanzarei/{0}_1.jpg'.format(img_name.split('.')[0]),rgb_img)
+		# cv2.circle(rgb_img,(x,y),r,(0,255,0),thickness=5)
+		# cv2.imwrite('/storage/ariyanzarei/{0}_1.jpg'.format(img_name.split('.')[0]),rgb_img)
 		return x,y,r,l,pname,coord
 	else:
 		return -1,-1,-1,-1,-1,-1
