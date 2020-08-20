b8d01c2751bf9db167c93813f898d49d6fbff72f
ariyanzri <ariyanzarei@email.arizona.edu>
2020-08-19 22:27:53 -0700

    test lids for season 11

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -979,6 +979,8 @@ def get_lid_in_patch(img_name,l,pname,coord,ransac_iter=500,ransac_min_num_fit=1
 
 	MB_size = int(77*settings.SCALE) if int(77*settings.SCALE) % 2 == 1 else int(77*settings.SCALE)+1
 	img  = cv2.medianBlur(img,MB_size)
+	img = 255-img
+	
 	# img = cv2.Canny(img,190,200)
 
 	# circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1.2, settings.LID_SIZE_AT_SCALE[0])
