49a7e67fda550f1c9b32c81213d8533b307daf7f
ariyanzri <ariyanzarei@email.arizona.edu>
2020-08-19 22:47:57 -0700

    test lids for season 11

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -980,7 +980,7 @@ def get_lid_in_patch(img_name,l,pname,coord,ransac_iter=500,ransac_min_num_fit=1
 	MB_size = int(77*settings.SCALE) if int(77*settings.SCALE) % 2 == 1 else int(77*settings.SCALE)+1
 	img  = cv2.medianBlur(img,MB_size)
 	img = 255-img
-	img = histogram_equalization(img)
+	# img = histogram_equalization(img)
 
 	# img = cv2.Canny(img,190,200)
 
@@ -996,7 +996,7 @@ def get_lid_in_patch(img_name,l,pname,coord,ransac_iter=500,ransac_min_num_fit=1
 
 	# get_lid_in_patch_with_SIFT(img,cv2.imread('/home/ariyanzarei/lid_image.jpg'),img_name)	
 
-	# cv2.imwrite('/storage/ariyanzarei/{0}_1.jpg'.format(img_name.split('.')[0]),img)
+	cv2.imwrite('/storage/ariyanzarei/{0}_1.jpg'.format(img_name.split('.')[0]),img)
 	# return -1,-1,-1,-1,-1,-1
 
 	# img = adjust_gamma(img,2.5)
@@ -1008,7 +1008,7 @@ def get_lid_in_patch(img_name,l,pname,coord,ransac_iter=500,ransac_min_num_fit=1
 	
 	(thresh, img) = cv2.threshold(img, t, 255, cv2.THRESH_BINARY)
 
-	cv2.imwrite('/storage/ariyanzarei/{0}_1.jpg'.format(img_name.split('.')[0]),img)
+	# cv2.imwrite('/storage/ariyanzarei/{0}_1.jpg'.format(img_name.split('.')[0]),img)
 	# cv2.namedWindow('a',cv2.WINDOW_NORMAL)
 	# cv2.resizeWindow('a',500,500)
 	# cv2.imshow('a',img)
