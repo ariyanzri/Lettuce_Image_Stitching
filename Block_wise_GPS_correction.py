eaf78ccdf5d126bec9781e50a98133f6f701c16e
ariyanzri <ariyanzarei@email.arizona.edu>
2020-08-31 16:17:07 -0700

    test season 11 later scans

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -976,13 +976,13 @@ def get_lid_in_patch(img_name,l,pname,coord,ransac_iter=500,ransac_min_num_fit=1
 		# img = histogram_equalization(img)
 		# img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)[:,:,2]
 
-	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	# hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-	lower_white = np.array([0,0,200], dtype=np.uint8)
-	upper_white = np.array([5,255,255], dtype=np.uint8)
+	# lower_white = np.array([0,0,200], dtype=np.uint8)
+	# upper_white = np.array([5,255,255], dtype=np.uint8)
 
-	mask = cv2.inRange(hsv, lower_white, upper_white)
-	img = cv2.bitwise_and(img,img, mask= mask).astype('uint8')
+	# mask = cv2.inRange(hsv, lower_white, upper_white)
+	# img = cv2.bitwise_and(img,img, mask= mask).astype('uint8')
 
 	# MB_size = int(77*settings.SCALE) if int(77*settings.SCALE) % 2 == 1 else int(77*settings.SCALE)+1
 	# img  = cv2.medianBlur(img,MB_size)
