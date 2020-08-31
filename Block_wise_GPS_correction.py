a256d0d599d31c94ca11e393376de71b9f9ab8d5
ariyanzri <ariyanzarei@email.arizona.edu>
2020-08-31 16:29:30 -0700

    test season 11 later scans

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -973,6 +973,7 @@ def get_lid_in_patch(img_name,l,pname,coord,ransac_iter=500,ransac_min_num_fit=1
 		img = cv2.imread('{0}/{1}'.format(settings.patch_folder,img_name))
 		img = cv2.resize(img,(int(img.shape[1]*settings.SCALE),int(img.shape[0]*settings.SCALE)))
 		rgb_img = img.copy()
+		cv2.imwrite('/storage/ariyanzarei/test/{0}_1.jpg'.format(img_name.split('.')[0]),img)
 		# img = histogram_equalization(img)
 		img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)[:,:,1]
 
@@ -997,7 +998,7 @@ def get_lid_in_patch(img_name,l,pname,coord,ransac_iter=500,ransac_min_num_fit=1
 
 	# get_lid_in_patch_with_SIFT(img,cv2.imread('/home/ariyanzarei/lid_image.jpg'),img_name)	
 
-	cv2.imwrite('/storage/ariyanzarei/test/{0}_1.jpg'.format(img_name.split('.')[0]),img)
+	
 	# return -1,-1,-1,-1,-1,-1
 
 	# img = adjust_gamma(img,2.5)
