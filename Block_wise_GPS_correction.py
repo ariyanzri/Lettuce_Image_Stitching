6399c91b38d7db74a4c0933571e898e92baa9917
ariyanzri <ariyanzarei@email.arizona.edu>
2020-09-02 15:58:41 -0700

    updating for late season 11 ortho

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -2394,7 +2394,7 @@ class Global_Optimizer:
 		transformation_coef_y = 1/(12.51*1e-6) 
 
 		GPS_coef_x = 1/(9.02*1e-5)
-		GPS_coef_y = 1/(10.48*1e-6)
+		GPS_coef_y = 1/(10.48*1e-5)
 		# GPS_coef_x = 1/(4.02*1e-5)
 		# GPS_coef_y = 1/(5.48*1e-5)
 		
