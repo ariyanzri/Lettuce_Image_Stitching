d989f8f38eba03336e675c74be82d9d86e982abc
ariyanzri <ariyanzarei@email.arizona.edu>
2020-09-01 17:11:48 -0700

    test season 11 lid

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -1072,7 +1072,7 @@ def get_lid_in_patch(img_name,l,pname,coord,ransac_iter=500,ransac_min_num_fit=1
 
 			if err <=settings.circle_error and r >= settings.LID_SIZE_AT_SCALE[0] and r <= settings.LID_SIZE_AT_SCALE[1]:
 				# cv2.putText(img, str(err), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA) 
-				# cv2.imwrite('/storage/ariyanzarei/test/{0}_1.jpg'.format(img_name.split('.')[0]),img)
+				cv2.imwrite('/storage/ariyanzarei/test/{0}_1.jpg'.format(img_name.split('.')[0]),img)
 				return x,y,r,l,pname,coord
 			else:
 				return -1,-1,-1,-1,-1,-1
@@ -4188,8 +4188,8 @@ class Field:
 			for p in g.patches:
 
 				for l in lids:
-					# if (p.gps.is_coord_inside(lids[l]) or p.gps.is_point_near(lids[l],2*settings.PATCH_SIZE_GPS[0])) and (p not in [ptch[0] for ptch in lid_patches]):
-					if p.gps.is_coord_inside(lids[l]) and (p not in [ptch[0] for ptch in lid_patches]):
+					if (p.gps.is_coord_inside(lids[l]) or p.gps.is_point_near(lids[l],2*settings.PATCH_SIZE_GPS[0])) and (p not in [ptch[0] for ptch in lid_patches]):
+					# if p.gps.is_coord_inside(lids[l]) and (p not in [ptch[0] for ptch in lid_patches]):
 						lid_patches.append((p,l))
 
 		return lid_patches
