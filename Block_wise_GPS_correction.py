899b3af45bce55a97c22adeb9c6f68fa0506a24b
ariyanzri <ariyanzarei@email.arizona.edu>
2020-09-01 17:58:15 -0700

    test season 11 lid

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -1119,7 +1119,7 @@ def get_lid_in_patch(img_name,l,pname,coord,ransac_iter=500,ransac_min_num_fit=1
 
 			if r >= settings.LID_SIZE_AT_SCALE[0] and r <= settings.LID_SIZE_AT_SCALE[1]:
 				
-				# cv2.imwrite('/storage/ariyanzarei/test/{0}_1.jpg'.format(img_name.split('.')[0]),img)
+				cv2.imwrite('/storage/ariyanzarei/test/{0}_1.jpg'.format(img_name.split('.')[0]),img)
 				return x,y,r,l,pname,coord
 			else:
 				return -1,-1,-1,-1,-1,-1
