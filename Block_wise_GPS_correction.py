cd784b13cca97a1897935391c0efcf430a2b2f22
ariyanzri <ariyanzarei@email.arizona.edu>
2020-09-07 21:18:12 -0700

    removing gps indivs and relying on row terms only

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -2437,16 +2437,16 @@ class Global_Optimizer:
 
 				A.append(row_y)
 				b.append(GPS_lids*p.gps.UL_coord[1])
-			# else:
-			# 	row_x = GPS_coef_x*template[self.image_name_to_index_dict[p.name],:]
-			# 	row_y = GPS_coef_y*template[self.number_of_images + self.image_name_to_index_dict[p.name],:]
+			else:
+				row_x = GPS_coef_x*template[self.image_name_to_index_dict[p.name],:]
+				row_y = GPS_coef_y*template[self.number_of_images + self.image_name_to_index_dict[p.name],:]
 
-			# 	A.append(row_x)
-			# 	b.append(GPS_coef_x*p.gps.UL_coord[0])
+				A.append(row_x)
+				b.append(GPS_coef_x*p.gps.UL_coord[0])
 				
 
-			# 	A.append(row_y)
-			# 	b.append(GPS_coef_y*p.gps.UL_coord[1])
+				# A.append(row_y)
+				# b.append(GPS_coef_y*p.gps.UL_coord[1])
 
 		# row fixating
 
