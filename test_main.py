4030897a73b042ff6d985988dd88e9a6a6efb0d0
ariyanzri <ariyanzarei@email.arizona.edu>
2020-09-09 13:23:14 -0700

    test on small portion

--- test_main.py
+++ test_main.py
@@ -37,24 +37,24 @@ def main(scan_date):
 	
 	field.draw_and_save_field(is_old=True)
 
-	# field.correct_field()
+	field.correct_field()
 
-	# field.save_new_coordinate()
+	field.save_new_coordinate()
 
-	# field.draw_and_save_field(is_old=False)
+	field.draw_and_save_field(is_old=False)
 
-	# new_lid_base_error = field.calculate_lid_based_error()
-	# new_RMSE = get_approximate_random_RMSE_overlap(field,20,settings.no_of_cores_to_use_max)
+	new_lid_base_error = field.calculate_lid_based_error()
+	new_RMSE = get_approximate_random_RMSE_overlap(field,20,settings.no_of_cores_to_use_max)
 
-	# print('------------------ ERROR MEASUREMENT ------------------ ')
+	print('------------------ ERROR MEASUREMENT ------------------ ')
 
 
-	# print('OLD Lid base Mean and Stdev: {0}'.format(old_lid_base_error))
-	# print('OLD SI: {0}'.format(np.mean(old_RMSE[:,3])))
+	print('OLD Lid base Mean and Stdev: {0}'.format(old_lid_base_error))
+	print('OLD SI: {0}'.format(np.mean(old_RMSE[:,3])))
 	
 
-	# print('NEW Lid base Mean and Stdev: {0}'.format(new_lid_base_error))
-	# print('NEW SI: {0}'.format(np.mean(new_RMSE[:,3])))
+	print('NEW Lid base Mean and Stdev: {0}'.format(new_lid_base_error))
+	print('NEW SI: {0}'.format(np.mean(new_RMSE[:,3])))
 
 
 def save_ortho():
