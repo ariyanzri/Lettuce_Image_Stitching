c07a1d1d2d36691a402a0db255fa62bff47a05e1
ariyanzri <ariyanzarei@email.arizona.edu>
2020-09-17 12:55:26 -0700

    updating for lid detection test

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -1072,7 +1072,7 @@ def get_lid_in_patch(img_name,l,pname,coord,ransac_iter=500,ransac_min_num_fit=1
 
 			if err <=settings.circle_error and r >= settings.LID_SIZE_AT_SCALE[0] and r <= settings.LID_SIZE_AT_SCALE[1]:
 				# cv2.putText(img, str(err), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA) 
-				# cv2.imwrite('/storage/ariyanzarei/test/{0}_1.jpg'.format(img_name.split('.')[0]),rgb_img)
+				cv2.imwrite('/storage/ariyanzarei/test/{0}.jpg'.format(img_name.split('.')[0]),rgb_img)
 				return x,y,r,l,pname,coord
 			else:
 				return -1,-1,-1,-1,-1,-1
@@ -4249,7 +4249,7 @@ class Field:
 					final_list_patches.append((patch,l,x,y))
 
 				# print(r/settings.SCALE,(r/settings.SCALE)*settings.GPS_TO_IMAGE_RATIO[0])
-				print('scp coge:/storage/ariyanzarei/season_11/2020-08-19/bin2tif_out/{0} .'.format(patch.name))
+				# print('scp coge:/storage/ariyanzarei/season_11/2020-08-19/bin2tif_out/{0} .'.format(patch.name))
 
 		print('Detected {0} lid patches in the field.'.format(len(final_list_patches)))
 		sys.stdout.flush()
