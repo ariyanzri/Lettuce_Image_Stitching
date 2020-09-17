d5cc10c9935a0fc3bd4252760dfac542664febf1
ariyanzri <ariyanzarei@email.arizona.edu>
2020-09-17 16:53:36 -0700

    updating for lid detection test

--- Block_wise_GPS_correction.py
+++ Block_wise_GPS_correction.py
@@ -1026,19 +1026,19 @@ def get_lid_in_patch(img_name,l,pname,coord,ransac_iter=500,ransac_min_num_fit=1
 
 			lid_img = cv2.resize(lid_img,(int(lid_img.shape[1]*settings.SCALE/settings.Height_Scale),int(lid_img.shape[0]*settings.SCALE/settings.Height_Scale)))
 			
-			# HSV_lid_img = cv2.cvtColor(lid_img,cv2.COLOR_BGR2HSV)
-			# Final_lid_img = np.zeros((HSV_lid_img.shape[0],HSV_lid_img.shape[1],2))
-			# Final_lid_img[:,:,0] = HSV_lid_img[:,:,0]
-			# Final_lid_img[:,:,1] = HSV_lid_img[:,:,1]
-			# Final_lid_img = np.mean(Final_lid_img,axis=-1).astype('uint8')
-			Final_lid_img = cv2.cvtColor(lid_img,cv2.COLOR_BGR2GRAY)
+			HSV_lid_img = cv2.cvtColor(lid_img,cv2.COLOR_BGR2HSV)
+			Final_lid_img = np.zeros((HSV_lid_img.shape[0],HSV_lid_img.shape[1],2))
+			Final_lid_img[:,:,0] = HSV_lid_img[:,:,0]
+			Final_lid_img[:,:,1] = HSV_lid_img[:,:,1]
+			Final_lid_img = np.mean(Final_lid_img,axis=-1).astype('uint8')
+			# Final_lid_img = cv2.cvtColor(lid_img,cv2.COLOR_BGR2GRAY)
 
-			# HSV_img=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-			# Final_img = np.zeros((HSV_img.shape[0],HSV_img.shape[1],2))
-			# Final_img[:,:,0] = HSV_img[:,:,0]
-			# Final_img[:,:,1] = HSV_img[:,:,1]
-			# Final_img = np.mean(Final_img,axis=-1).astype('uint8')
-			Final_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+			HSV_img=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+			Final_img = np.zeros((HSV_img.shape[0],HSV_img.shape[1],2))
+			Final_img[:,:,0] = HSV_img[:,:,0]
+			Final_img[:,:,1] = HSV_img[:,:,1]
+			Final_img = np.mean(Final_img,axis=-1).astype('uint8')
+			# Final_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 			result=cv2.matchTemplate(Final_img,Final_lid_img,cv2.TM_CCOEFF)
 			sin_val, max_val, min_loc, max_loc=cv2.minMaxLoc(result)
