9dedfde3314c728668f19ebdb776c19a25b6d1f6
ariyanzri <ariyanzarei@email.arizona.edu>
2020-11-05 12:12:47 -0700

    updating the get lid image names script

--- get_lids_image_names.py
+++ get_lids_image_names.py
@@ -9,16 +9,18 @@ lid_locations = pd.read_csv(csv_lids_locations_path)
 
 scan_coords_csv = os.listdir(csv_locations_path)
 
+final_list_associated = []
+
 for scan in scan_coords_csv:
+	scan_name = scan.replace('_coordinates.csv','')
 	scan_data = pd.read_csv('{0}/{1}'.format(csv_locations_path,scan))
 
 	for lid_index,lid in lid_locations.iterrows():
 		
 		lid_name = lid['lid_id']
-		lid_lat = lid['lat']
-		lid_lon = lid['lon']
+		lid_loc = {'lat':lid['lat'],'lon':lid['lon']}
 
-		# print('{0} - {1} - {2}'.format(lid_name,lid_lat,lid_lon))
+		# print('{0} - {1} - {2}'.format(lid_name,lid_loc['lat'],lid_loc['lon']))
 
 		for img_index,img_data in scan_data.iterrows():
 
@@ -35,4 +37,10 @@ for scan in scan_coords_csv:
 			img_LR = {'lat':LR[1],'lon':LR[0]}
 			img_C = {'lat':C[1],'lon':C[0]}
 
-			print('\t {0} - {1} - {2} - {3} - {4} - {5}'.format(img_name,img_UL,img_UR,img_LL,img_LR,img_C))
+			# print('\t {0} - {1} - {2} - {3} - {4} - {5}'.format(img_name,img_UL,img_UR,img_LL,img_LR,img_C))
+
+			if lid_loc['lon']>=img_UL['lon'] and lid_loc['lon']<=img_UR['lon'] and lid_loc['lat']<=img_UL['lat'] and lid_loc['lat']>=img_LL['lat']:
+				final_list_associated.append((scan_name,lid_name,img_name))
+
+for item in final_list_associated:
+	print(item)
