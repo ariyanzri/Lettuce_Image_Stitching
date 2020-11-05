76f693c85c1ddc3999d705f13dc662f40cc70407
ariyanzri <ariyanzarei@email.arizona.edu>
2020-11-05 14:30:31 -0700

    run on hpc

--- get_lids_image_names.py
+++ get_lids_image_names.py
@@ -24,73 +24,72 @@ def read_csv_to_dict(path):
 			
 
 lid_locations = read_csv_to_dict(csv_lids_locations_path)
-print(lid_locations)
 
-# scan_coords_csv = os.listdir(csv_locations_path)
+scan_coords_csv = os.listdir(csv_locations_path)
 
-# final_list_associated = {}
+final_list_associated = {}
 
-# scans = {}
+scans = {}
 
-# for scan in scan_coords_csv:
-# 	scan_name = scan.replace('_coordinates.csv','')
-# 	scan_data = read_csv_to_dict('{0}/{1}'.format(csv_locations_path,scan))
+for scan in scan_coords_csv:
+	scan_name = scan.replace('_coordinates.csv','')
+	scan_data = read_csv_to_dict('{0}/{1}'.format(csv_locations_path,scan))
 
-# 	scans[scan_name] = scan_data
+	scans[scan_name] = scan_data
 
-# for scan_name in scans:
+for scan_name in scans:
 
-# 	scan_data = scans[scan_name]
-# 	final_list_associated[scan_name] = {}
-# 	final_list_associated[scan_name]['path'] = '/iplant/home/shared/terraref/ua-mac/level_1/season_10_yr_2020/stereoTop/{0}/{0}_bin2tif.tar.gz'.format(scan_name)
-# 	final_list_associated[scan_name]['images'] = []
+	scan_data = scans[scan_name]
+	final_list_associated[scan_name] = {}
+	final_list_associated[scan_name]['path'] = '/iplant/home/shared/terraref/ua-mac/level_1/season_10_yr_2020/stereoTop/{0}/{0}_bin2tif.tar.gz'.format(scan_name)
+	final_list_associated[scan_name]['images'] = []
 
-# 	for lid in lid_locations:
+	for lid in lid_locations:
 		
-# 		lid_name = lid['lid_id']
-# 		lid_loc = {'lat':float(lid['lat']),'lon':float(lid['lon'])}
+		lid_name = lid['lid_id']
+		lid_loc = {'lat':float(lid['lat']),'lon':float(lid['lon'])}
 
-# 		for img_data in scan_data:
+		for img_data in scan_data:
 
-# 			img_name = img_data['Filename']
-# 			UL = img_data['Upper left'].replace('"','').split(',')
-# 			UR = img_data['Upper right'].replace('"','').split(',')
-# 			LL = img_data['Lower left'].replace('"','').split(',')
-# 			LR = img_data['Lower right'].replace('"','').split(',')
-# 			C = img_data['Center'].replace('"','').split(',')
+			img_name = img_data['Filename']
+			UL = img_data['Upper left'].replace('"','').split(',')
+			UR = img_data['Upper right'].replace('"','').split(',')
+			LL = img_data['Lower left'].replace('"','').split(',')
+			LR = img_data['Lower right'].replace('"','').split(',')
+			C = img_data['Center'].replace('"','').split(',')
 
-# 			img_UL = {'lat':float(UL[1]),'lon':float(UL[0])}
-# 			img_UR = {'lat':float(UR[1]),'lon':float(UR[0])}
-# 			img_LL = {'lat':float(LL[1]),'lon':float(LL[0])}
-# 			img_LR = {'lat':float(LR[1]),'lon':float(LR[0])}
-# 			img_C = {'lat':float(C[1]),'lon':float(C[0])}
+			img_UL = {'lat':float(UL[1]),'lon':float(UL[0])}
+			img_UR = {'lat':float(UR[1]),'lon':float(UR[0])}
+			img_LL = {'lat':float(LL[1]),'lon':float(LL[0])}
+			img_LR = {'lat':float(LR[1]),'lon':float(LR[0])}
+			img_C = {'lat':float(C[1]),'lon':float(C[0])}
 
-# 			if lid_loc['lon']>=img_UL['lon'] and lid_loc['lon']<=img_UR['lon'] and lid_loc['lat']<=img_UL['lat'] and lid_loc['lat']>=img_LL['lat']:
-# 				final_list_associated[scan_name]['images'].append(img_name)
+			if lid_loc['lon']>=img_UL['lon'] and lid_loc['lon']<=img_UR['lon'] and lid_loc['lat']<=img_UL['lat'] and lid_loc['lat']>=img_LL['lat']:
+				final_list_associated[scan_name]['images'].append(img_name)
 
-# print('>>> Associated images with lids have been detected.')
+print('>>> Associated images with lids have been detected.')
 
-# for scan_name in final_list_associated:
+for scan_name in final_list_associated:
 
-# 	param1 = final_list_associated[scan_name]['path']
-# 	param2 = path_to_download
-# 	param3 = '{0}_tarfile.tar.gz'.format(scan_name)
+	param1 = final_list_associated[scan_name]['path']
+	param2 = path_to_download
+	param3 = '{0}_tarfile.tar.gz'.format(scan_name)
 
-# 	process = subprocess.Popen(['. {0}/download_untar.sh'.format(base_path),param1,param2,param3],shell=True)
-# 	process.wait()
+	process = subprocess.Popen(['. {0}/download_untar.sh'.format(base_path),param1,param2,param3],shell=True)
+	process.wait()
 
-# 	print('>>> Raw images download and the tar file successfully untarred.')
+	print('>>> Raw images download and the tar file successfully untarred.')
 
-# 	for i,img_name in enumerate(final_list_associated[scan_name]['images']):
-# 		src = '{0}/bin2tif_out/{1}'.format(path_to_download,img_name)
-# 		dst = '{0}/{1}_{2}.tif'.format(path_final_original,scan_name,img_name)
-# 		copyfile(src, dst)
+	for i,img_name in enumerate(final_list_associated[scan_name]['images']):
+		src = '{0}/bin2tif_out/{1}'.format(path_to_download,img_name)
+		dst = '{0}/{1}_{2}.tif'.format(path_final_original,scan_name,img_name)
+		copyfile(src, dst)
 
-# 	print('>>> Lid images successfully moved to the proper directories.')
+	print('>>> Lid images successfully moved to the proper directories.')
 
-# 	process = subprocess.Popen(['rm','-r','{0}/*'.format(path_to_download)])
-# 	process.wait()
+	process = subprocess.Popen(['rm','-r','{0}/*'.format(path_to_download)])
+	process.wait()
 
-# 	print('>>> Downloaded files deleted successfully.')
+	print('>>> Downloaded files deleted successfully.')
 
-# 	break
+	break
