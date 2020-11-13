6fdd8b0c624d7b43df2e0f50d00678b920d907a8
ariyanzri <ariyanzarei@email.arizona.edu>
2020-11-12 21:09:09 -0700

    up

--- get_lids_image_names.py
+++ get_lids_image_names.py
@@ -76,42 +76,43 @@ for scan_name in scans:
 			if lid_loc['lon']>=img_UL['lon'] and lid_loc['lon']<=img_UR['lon'] and lid_loc['lat']<=img_UL['lat'] and lid_loc['lat']>=img_LL['lat']:
 				final_list_associated[scan_name]['images'].append(img_name)
 
+print(final_list_associated)
 print('>>> Associated images with lids have been detected.')
 
-for scan_name in final_list_associated:
+# for scan_name in final_list_associated:
 
-	print('>>> Start processing scan date {0}'.format(scan_name))
+# 	print('>>> Start processing scan date {0}'.format(scan_name))
 
-	param1 = final_list_associated[scan_name]['path']
-	param2 = path_to_download
-	param3 = '{0}_tarfile.tar'.format(scan_name)
+# 	param1 = final_list_associated[scan_name]['path']
+# 	param2 = path_to_download
+# 	param3 = '{0}_tarfile.tar'.format(scan_name)
 
-	process = subprocess.Popen(['. {0}/download_untar.sh'.format(base_path),param1,param2,param3],shell=True)
-	process.wait()
+# 	process = subprocess.Popen(['. {0}/download_untar.sh'.format(base_path),param1,param2,param3],shell=True)
+# 	process.wait()
 
-	print('>>> Raw images download and the tar file successfully untarred.')
+# 	print('>>> Raw images download and the tar file successfully untarred.')
 
-	for i,img_name in enumerate(final_list_associated[scan_name]['images']):
-		src = '{0}/bin2tif_out/{1}'.format(path_to_download,img_name)
-		dst = '{0}/{1}_{2}'.format(path_final_original,scan_name,img_name)
-		dst2 = '{0}/{1}_{2}'.format(path_final_down_scaled,scan_name,img_name)
+# 	for i,img_name in enumerate(final_list_associated[scan_name]['images']):
+# 		src = '{0}/bin2tif_out/{1}'.format(path_to_download,img_name)
+# 		dst = '{0}/{1}_{2}'.format(path_final_original,scan_name,img_name)
+# 		dst2 = '{0}/{1}_{2}'.format(path_final_down_scaled,scan_name,img_name)
 		
-		if os.path.exists(src):
+# 		if os.path.exists(src):
 			
-			copyfile(src, dst)
+# 			copyfile(src, dst)
 
-			image_large = cv2.imread(src)
-			image_small = cv2.resize(image_large,(int(0.2*image_large.shape[1]),int(0.2*image_large.shape[0])))
+# 			image_large = cv2.imread(src)
+# 			image_small = cv2.resize(image_large,(int(0.2*image_large.shape[1]),int(0.2*image_large.shape[0])))
 
-			cv2.imwrite(dst2,image_small)
+# 			cv2.imwrite(dst2,image_small)
 
-	print('>>> Lid images successfully moved to the proper directories.')
+# 	print('>>> Lid images successfully moved to the proper directories.')
 
-	rmtree(path_to_download)
-	os.mkdir(path_to_download)
+# 	rmtree(path_to_download)
+# 	os.mkdir(path_to_download)
 
-	print('>>> Downloaded files deleted successfully.')
+# 	print('>>> Downloaded files deleted successfully.')
 
-	print('>>> Finished processing scan date {0}'.format(scan_name))
+# 	print('>>> Finished processing scan date {0}'.format(scan_name))
 
-	print('--------------------------------------------------------------')
+# 	print('--------------------------------------------------------------')
