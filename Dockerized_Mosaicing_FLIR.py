621c28dfa194ba131802eae6e06d79da267cc9b0
ariyanzri <ariyanzarei@email.arizona.edu>
2021-01-10 12:35:38 -0700

    update for separating mosaicing and gps correction FLIR

--- Dockerized_Mosaicing_FLIR.py
+++ Dockerized_Mosaicing_FLIR.py
@@ -26,28 +26,8 @@ def main(scan_date):
 
 	field = Field(is_single_group=settings.is_single_group)
 	
-	field.save_plot()
-	
-	# old_RMSE = get_approximate_random_RMSE_overlap(field,10,settings.no_of_cores_to_use_max)
-
-	field.create_patches_SIFT_files()
-	
-	# field.draw_and_save_field(is_old=True)
-
-	field.correct_field()
-
-	field.save_new_coordinate()
-
-	# field.draw_and_save_field(is_old=False)
-
-	# new_RMSE = get_approximate_random_RMSE_overlap(field,10,settings.no_of_cores_to_use_max)
-
-	# print('------------------ ERROR MEASUREMENT ------------------ ')
-
-	# print('OLD SI: {0}'.format(np.mean(old_RMSE[:,3])))
-	
-	# print('NEW SI: {0}'.format(np.mean(new_RMSE[:,3])))
-
+	field.draw_and_save_field(is_old=True)
+	field.draw_and_save_field(is_old=False)
 
 def get_args():
 
